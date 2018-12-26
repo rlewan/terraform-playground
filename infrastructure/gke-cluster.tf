9d4ab2eecc13eef17bf7e878a7f972d72dda548a
Rafał Lewandowski <rafal.lewandowski@swingdev.io>
2018-12-27 00:00:08 +0100

    Enable cluster dasboard

--- infrastructure/gke-cluster.tf
+++ infrastructure/gke-cluster.tf
@@ -2,4 +2,10 @@ resource "google_container_cluster" "gke-cluster-resource" {
   name = "gke-cluster"
   zone = "europe-west1-b"
   initial_node_count = 3
+
+  addons_config {
+    kubernetes_dashboard {
+      disabled = false
+    }
+  }
 }
